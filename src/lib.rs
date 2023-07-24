d951ada9ba7e2f0e5234397f9c5553e15db58855
Exotik850 <kidkool850@gmail.com>
2023-07-24 15:29:53 -0500

    Added better display with serde_json and qb_id for api calls

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,17 @@ extern crate derive_builder;
 pub mod models;
 use models::*;
 use models::common::MetaData;
+use serde::Serialize;
+use std::fmt::Display;
 
-pub trait HasQBData {
+pub trait HasQBData 
+where Self: Serialize
+{
     fn id(&self) -> Option<String>;
     fn sync_token(&self) -> Option<String>;
     fn meta_data(&self) -> Option<MetaData>;
     fn name(&self) -> &str;
+    fn qb_id(&self) -> String;
 }
 
 macro_rules! impl_qb_data {
@@ -29,8 +34,20 @@ macro_rules! impl_qb_data {
                     self.meta_data.clone()
                 }
 
+                #[inline]
                 fn name(&self) -> &str {
                     stringify!($x)
+                }
+
+                #[inline]
+                fn qb_id(&self) -> String {
+                    self.name().to_lowercase()
+                }
+            }
+
+            impl Display for $x {
+                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                    write!(f, "{} : {}", self.name(), serde_json::to_string_pretty(self).unwrap())
                 }
             }
         )+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use quickbooks_types::models::VendorBuilder;
+use quickbooks_types::{models::VendorBuilder};
 
 fn main() {
     let ven = VendorBuilder::default().title("Nana").build().unwrap();
-    println!("{ven:?}")
+    println!("{ven}");
+    println!("{:?}", ven);
 }
