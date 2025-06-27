d90897cd5b42651981866173849014b6d0fd0cda
Exotik850 <kidkool850@gmail.com>
2025-06-27 11:35:24 -0500

    remove redundant cfg attribute from MissingField variant in QBTypeError

--- src/error.rs
+++ src/error.rs
@@ -3,7 +3,6 @@ pub enum QBTypeError {
     #[cfg(feature = "builder")]
     #[error("Error validating QB Object in builder: {0}")]
     ValidationError(String),
-    #[cfg(feature = "builder")]
     #[error("Missing field in QB Object: {0}")]
     MissingField(&'static str),
     #[error("QB Item could not be referenced!")]
