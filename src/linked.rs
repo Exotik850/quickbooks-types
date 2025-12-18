74197e432233a915ac399f1e8aaf5073af24916a
kidkool850@gmail.com <kidkool850@gmail.com>
2025-12-18 09:44:50 -0600

    add linkedto trait and macro for easy impl

--- src/linked.rs
+++ src/linked.rs
@@ -1,12 +1,33 @@
 use crate::common::TypedRef;
-use crate::models::*;
 
-pub trait Linked<O> {
-    fn linked_ref(&self) -> Option<&TypedRef<O>>;
+// we can represent the child -> parent relationship for now,
+// parent -> children requires querying the API, which is out of scope here
+
+/// Trait for "this object has a single linked object of type `Target`".
+///
+/// Returns an optional reference to the linked object.
+pub trait LinkedTo<Target> {
+    fn linked(&self) -> Option<&TypedRef<Target>>;
 }
 
-impl Linked<Customer> for Invoice {
-    fn linked_ref(&self) -> Option<&TypedRef<Customer>> {
-        self.customer_ref.as_ref()
-    }
+#[macro_export]
+macro_rules! impl_linked {
+    ($source:ty as $field:tt => $target:ty) => {
+        ::paste::paste! {
+            impl $crate::linked::LinkedTo<$target> for $source {
+                fn linked(&self) -> Option<&TypedRef<$target>> {
+                    self.$field.as_ref()
+                }
+            }
+        }
+    };
+    ($source:ty => $target:ty) => {
+        ::paste::paste! {
+            impl $crate::linked::LinkedTo<$target> for $source {
+                fn linked(&self) -> Option<&TypedRef<$target>> {
+                    self.[<$target:snake:lower _ref>].as_ref()
+                }
+            }
+        }
+    };
 }
